f71455ea3e7f425c45c06b6384117d401ebdec46
unknown <andile.goba@younglings.africa>
2024-09-11 13:20:42 +0200

    breakingnews, pagination, slideritem files update

---
+++ components/Pagination.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NewsDataType } from '@/types';
+import Animated, { SharedValue } from 'react-native-reanimated';
+import { Colors } from '@/constants/Colors';
+
+type Props = {
+    items: NewsDataType[],
+    paginationIndex: number,
+    scrollX: SharedValue<number>,
+}
+
+const Pagination = ({items, paginationIndex, scrollX}: Props) => {
+    return(
+        <View style={styles.container}>
+            {items.map((_, index) => {
+                return<Animated.View style={[styles.dot, {backgroundColor: paginationIndex === index ? Colors.tint : Colors.darkGrey}]} key={index}/>
+            })}
+        </View>
+    )
+}
+
+export default Pagination;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        height: 40,
+    }
+
+});
