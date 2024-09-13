04b6e3d5d82a6ff947af8192b7f3b44ae14b00a6
unknown <andile.goba@younglings.africa>
2024-09-13 10:49:53 +0200

    search, newlist file update

--- app/news/search.tsx
+++ app/news/search.tsx
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import { FlatList, StyleSheet,TouchableOpacity, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useLocalSearchParams } from 'expo-router';
+import { Link, router, Stack, useLocalSearchParams } from 'expo-router';
 import { NewsDataType } from '@/types';
+import { Ionicons } from '@expo/vector-icons';
+import Loading from '@/components/Loading';
+import { NewsItem } from '@/components/NewsList';
 
 type Props = {};
 
@@ -10,6 +13,10 @@ const Search = (props: Props) => {
     const {query, category, country} = useLocalSearchParams<{query: string, category: string, country: string}>();
     const [news, setNews] = useState<NewsDataType[]>([]);
     const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        getNews();
+    },[])
 
   const getNews = async (category:string = '') => {
     try {
@@ -25,7 +32,7 @@ const Search = (props: Props) => {
       if(query){
         queryString = `&q=${query}`
       }
-      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=10${categoryString}`;
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=10${categoryString}${countryString}${queryString}`;
       const response = await axios .get(URL);
 
       //console.log("News data: ", response.data);
@@ -38,14 +45,41 @@ const Search = (props: Props) => {
     }
   }
     return(
-        <View>
-            <Text>Search Query: {query}</Text>
-            <Text>Category: {category}</Text>
-            <Text>Country: {country}</Text>
-        </View>
+        <>
+            <Stack.Screen options={{
+                headerLeft: () => (
+                    <TouchableOpacity onPress={() => router.back()}>
+                        <Ionicons name='arrow-back' size={22}/>
+                    </TouchableOpacity>
+                ),
+                title: "Search"
+            }}
+            />
+            <View style={styles.container}>
+                {isLoading ? (
+                    <Loading size={'large'} />
+                ): (
+                    <FlatList data={news} keyExtractor={(_, index) => `list_item${index}`} showsVerticalScrollIndicator={false} renderItem={({index, item}) => {
+                        return(
+                            <Link href={`/news/${item.article_id}`} asChild key={index}>
+                                <TouchableOpacity>
+                                    <NewsItem item={item} />
+                                </TouchableOpacity>
+                            </Link>
+                        )
+                    }}/>
+                )}
+            </View>
+        </>
     )
 }
 
 export default Search;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginHorizontal: 20,
+        marginVertical: 20,
+    }
+});
